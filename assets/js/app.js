c0844142414215cacf909c4746f5189e523ab2af
Jeetendra Choudhary <mailme@Jeetendra.xyz>
2016-05-16 23:08:05 +0530

    Identity Experiment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,47 @@
 var app = {};
 
 app.configureCognito = function(){
-	AWS.config.region = 'us-east-1';
-	AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-	    IdentityPoolId: 'YOUR_IDENTITY_POOL_ID',
-	});	
-	console.log(AWS.config.credentials);
+	AWSCognito.config.region = 'us-east-1';
+
+	var poolData = {
+	    UserPoolId: 'userPoolId',
+	    ClientId: 'ClientId'
+	};
+
+	var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+	var userData = {
+			UserName:'MyName',
+			Pool: userPool
+	};
+
+	var attributeList = [];
+
+	var dataEmail = {
+		Name: 'email',
+		Value: 'mailme@mydomain.com'
+	};
+
+	var dataPhoneNumber = {
+		Name: 'phone_number',
+		Value: '+9112212212212'
+	};
+
+	var attributeEmail = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataEmail);
+	var attributePhoneNumber = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataPhoneNumber);
 	
-	AWS.config.credentials.get(function(){
-		var syncClient = new AWS.CognitoSyncManager();
-		console.log(syncClient);
-		syncClient.openOrCreateDataset('myDataset',function(err,dataset){
-			dataset.put('myKey','myValue',function(err,record){
-				dataset.synchronize({
-					onSuccess: function(data,newRecords){
-						// Handler Code Here....
-						console.log(data);
-					}
-				});
-			});	
-		});	
-	});	
-}
+	attributeList.push(attributeEmail);
+	attributeList.push(attributePhoneNumber);
+
+	var cognitoUser;
+
+	userPool.signUp('userName','password',attributeList,null,function(err,result){
+		if(err){
+			console.log(err);
+			alert(err);
+			return;
+		}
+		cognitoUser = result.user;
+		console.log('User Name is: '+cognitoUser);
+	});
+	
+};
